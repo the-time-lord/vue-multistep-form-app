aebf66195288c98ae6dd6e44e616b6d03c9a03c9
Daria Katysheva <Daria.Katysheva@Student.HTW-Berlin.de>
2024-02-19 11:58:37 +0100

    Redirect to Home when clicking on the logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import IconDocument from './icons/IconDocument.vue'
+
+const router = useRouter()
 </script>
 
 <template>
   <header class="h-14 flex items-center">
-    <div class="flex items-center pl-3 gap-2">
+    <div class="flex items-center pl-3 gap-2 cursor-pointer" @click="() => router.push('/')">
       <IconDocument />
       <h1 class="text-2xl font-semibold uppercase">Multistep Form</h1>
     </div>
